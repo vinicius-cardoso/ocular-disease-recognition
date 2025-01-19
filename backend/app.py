e541ec13feeaa998ce281d3c90d1538b44d3116a
Henrique Perim <henriqued.perim@gmail.com>
2025-01-19 17:56:25 -0300

    feat: refactor, nginx and cloudflare

--- backend/app.py
+++ backend/app.py
@@ -32,5 +32,6 @@ def index():
 
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
